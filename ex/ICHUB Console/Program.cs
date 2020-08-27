2e5f1343e71910704df9442c4a9ab21e2fc3f1ca
Bach Dang <haidang207@gmail.com>
2020-08-27 10:21:06 +0700

    Ex for console

---
+++ ex/ICHUB Console/Program.cs
@@ -0,0 +1,58 @@
+﻿using ICHUB_LIBRARY;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ICHUB_LIBRARY.Models;
+
+namespace ICHUB_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Tao đối tượng kết nối truyền vào accesskey của project trên app ICHUB
+                ConnectICHUB connectICHUB = new ConnectICHUB("EWXD111");
+                //bắt sự kiện khi có thay đổi
+                connectICHUB.DataChange += ChangeData;
+                connectICHUB.ErrorArgs += ErrorErgs;
+                Console.ReadKey();
+            }
+            catch (Exception s)
+            {
+                Console.WriteLine(s.Message);
+            }
+        }
+        // Khi dữ liệu thay đổi sự kiện DataChangeArgs sẽ được gọi
+        // Chú ý sự kiện chạy ở 1 luồn riêng
+        static void ChangeData(object sender, DataChangeArgs e)
+        {
+            try
+            {
+                //Phân tích dữ liệu nhận dc
+                foreach(var item in e.Data)
+                {
+                    Console.WriteLine("Tên:" + item.Name);
+                    Console.WriteLine("Data:"+item.Data );                
+                }
+                Console.WriteLine("--------------------");
+            }
+            catch
+            {
+
+            }
+
+
+        }
+        // Khi phát sinh lỗi sự kiện ErrorArgs sẽ được gọi
+        static void ErrorErgs(object senser, ErrorArgs e)
+        {
+            Console.WriteLine(JsonConvert.SerializeObject(e.Error.Detail));
+        }
+
+    }
+}
--- ex/ICHUB Console/Properties/AssemblyInfo.cs
+++ ex/ICHUB Console/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp6")]
+[assembly: AssemblyTitle("ICHUB Console")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp6")]
+[assembly: AssemblyProduct("ICHUB Console")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("942fa477-6a41-4dc7-8d0a-72629113712c")]
+[assembly: Guid("b2b38e22-6cee-4625-8056-8bcf7502f29c")]
 
 // Version information for an assembly consists of the following four values:
 //
